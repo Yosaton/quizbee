9c1627ba9e341803eb0398fbd83402a96aff6964
yosaton <Yosaton@Yo.com>
2020-05-06 04:21:13 -0500

    Updates

---
+++ precache-manifest.54bd1359903f877d60501c89ea1f27ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932fa37982c4ccf60bf3936e3a775862",
+    "url": "/vetibros/index.html"
+  },
+  {
+    "revision": "c114fb6a383f58d78e17",
+    "url": "/vetibros/static/css/main.4cdb80c0.chunk.css"
+  },
+  {
+    "revision": "bd4bde72137969b3ae52",
+    "url": "/vetibros/static/js/2.ee439d6b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/vetibros/static/js/2.ee439d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c114fb6a383f58d78e17",
+    "url": "/vetibros/static/js/main.ea26b057.chunk.js"
+  },
+  {
+    "revision": "707a645cdeb4644ba091",
+    "url": "/vetibros/static/js/runtime-main.846ff067.js"
+  },
+  {
+    "revision": "c301831e50eb0d4e12566d5dd0e91688",
+    "url": "/vetibros/static/media/granite.c301831e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quizbee/precache-manifest.9c9b76b08948603baae712c722150c23.js"
+  "/vetibros/precache-manifest.54bd1359903f877d60501c89ea1f27ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quizbee/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vetibros/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
